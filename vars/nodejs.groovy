76b3b793fc34e87f46ccee3f729ec362825ff29f
madhaviaws <madhavi.oct14@gmail.com>
2022-10-07 16:41:32 +0530

    shared libraries changes

---
+++ vars/nodejs.groovy
@@ -0,0 +1,40 @@
+def lintCheck() {
+    sh ''' 
+         # We want Devs to handle the lint checks failure 
+         # npm i jslint 
+         # node_modules/jslint/bin/jslint.js  server.js || true 
+         echo Starting lint checks
+         echo Lint Checks Completed for ${COMPONENT}
+    ''' 
+}
+
+
+pipeline {
+    agent any 
+        stages{
+            stage ('Downloading the Dependencies') {
+              steps{
+                //sh "npm install"
+                sh "npm installation"
+                }
+            }
+
+            stage ('Lint Checks'){
+                steps {
+                    // script{
+                      //  sample.info("Welcome","stockexchange.com
+                        //")
+                     //}
+                     script{
+                        lintCheck()
+                     }
+                    //sh "echo installing jslint"
+                    //sh "npm i jslint"
+                    //sh "node_modules/jslint/bin/jslint.js server.js"
+                   // sh "echo lint checks has completed"
+                    
+                }      
+            }
+        }
+    
+}
---
+++ vars/sample.groovy
@@ -0,0 +1,13 @@
+// def info(message) {                 // Declaring a funciton in groovy 
+//     echo "INFO: ${message}"
+// }
+
+
+
+// info("Hai")  // Calling a function 
+
+def info(message, URL) {                 // Declaring a funciton in groovy 
+    echo "INFO: ${message} , ${URL}"
+}
+
+info("Hai" , "google.com")  // Calling a function 
