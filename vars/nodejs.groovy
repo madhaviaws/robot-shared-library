a41b3e01b82f7d4d467151f011781927b47df9bb
madhaviaws <madhavi.oct14@gmail.com>
2023-02-22 11:44:41 +0530

    ci

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,85 +1,85 @@
-def call() {
-    node {
-        git branch: 'main', url: "https://github.com/madhaviaws/${COMPONENT}.git"
-        env.APPTYPE="nodejs"
-        env.ARGS="-Dsonar.sources=."
-        common.sonarCheck()
-        common.lintCheck()
-        common.testCases()
-        if (env.TAG_NAME != null) {
-            common.artifact()
-        }
-    }
-}
+// def call() {
+//     node {
+//         git branch: 'main', url: "https://github.com/madhaviaws/${COMPONENT}.git"
+//         env.APPTYPE="nodejs"
+//         env.ARGS="-Dsonar.sources=."
+//         common.sonarCheck()
+//         common.lintCheck()
+//         common.testCases()
+//         if (env.TAG_NAME != null) {
+//             common.artifact()
+//         }
+//     }
+// }
 
-//def lintCheck() {
-  //  sh ''' 
+def lintCheck() {
+  sh ''' 
     //     # We want Devs to handle the lint checks failure 
       //   # npm i jslint 
         // # node_modules/jslint/bin/jslint.js  server.js || true 
-         //echo Starting lint checks
-        // echo Lint Checks Completed for ${COMPONENT}
-    //''' 
-//}
+         echo Starting lint checks
+         echo Lint Checks Completed for ${COMPONENT}
+    ''' 
+}
 
-//def call() {
-//pipeline {
-  //  agent any 
-   //  environment {
-     //   SONAR      = credentials('SONAR')
-       //  NEXUS      = credentials('NEXUS')
-    //}
-      //  stages{
-        //    stage ('Downloading the Dependencies') {
-          //    steps{
-                //sh "npm install"
-            //    sh "echo npm installation"
-              //  }1
-            //}
+def call() {
+pipeline {
+    agent any 
+     environment {
+        SONAR      = credentials('SONAR')
+       NEXUS      = credentials('NEXUS')
+    }
+      stages{
+        stage ('Downloading the Dependencies') {
+              steps{
+                 sh "npm install"
+                 sh "echo npm installation"
+                }
+            }
 
-            //stage ('Lint Checks'){
-              //  steps {
-                    // script{
-                      //  sample.info("Welcome","stockexchange.com
+            stage ('Lint Checks'){
+              steps {
+                     //script{
+                       // sample.info("Welcome","stockexchange.com
                         //")
                      //}
-                ////     script{
-                    //    lintCheck()
-                    // }
+                     script{
+                    lintCheck()
+                     }
                     //sh "echo installing jslint"
                     //sh "npm i jslint"
                     //sh "node_modules/jslint/bin/jslint.js server.js"
                    // sh "echo lint checks has completed"
                     
-                //}      
-            //}
-            //stage('Sonar Check') {
-              //  steps {
-                //    script { 
-                  //      env.ARGS="-Dsonar.sources=."
-                    //    common.sonarCheck()
-                   // }
-               // }
-            //}
-             //stage('Test Cases') {
-            //parallel {
-              //  stage('Unit Tests') {
-                //    steps {
-                  //      sh 'echo Unit Test Cases Completed'
-                    //     }
-                    //}
-                //stage('Integration Tests') {
-                  //  steps {
-                    //    sh 'echo Integration Test Cases Completed'
-                      //   }
-                    //}
-                //stage('Functional Tests') {
-                  //  steps {
-                    //    sh 'echo Functional Test Cases Completed'
-                      //   }
-                    //}
-               // }
-            //}
+                }      
+            }
+            stage('Sonar Check') {
+              steps {
+                    script { 
+                        env.ARGS="-Dsonar.sources=."
+                      common.sonarCheck()
+                   }
+                }
+            }
+            stage('Test Cases') {
+            parallel {
+                stage('Unit Tests') {
+                  steps {
+                    sh 'echo Unit Test Cases Completed'
+                         }
+                    }
+                stage('Integration Tests') {
+                  steps {
+                    sh 'echo Integration Test Cases Completed'
+                   }
+                }
+                stage('Functional Tests') {
+                  steps {
+                    sh 'echo Functional Test Cases Completed'
+                   }
+                }
+               }
+            }
 
             //             stage('Check the release') {
 //                 when {
@@ -119,7 +119,7 @@ def call() {
 //                     '''
       //          }
     //        }
-  //      }
+       }
     
-//}
-//}
+}
+}
