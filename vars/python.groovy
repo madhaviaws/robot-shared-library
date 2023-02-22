c350cb07879e2c572ac023803849052800b75b75
madhaviaws <madhavi.oct14@gmail.com>
2023-02-22 12:27:14 +0530

    ci

--- vars/python.groovy
+++ vars/python.groovy
@@ -16,7 +16,7 @@
 def lintCheck() {
     sh ''' 
          echo Starting lint checks ${COMPONENT}
-      //   # pylint *.py           # lint checks
+      //    pylint *.py           # lint checks
          echo Lint Checks Completed for ${COMPONENT}
        
        ''' 
