d7155bc7040bc2724d486e558e6f7696c002eb1e
madhaviaws <madhavi.oct14@gmail.com>
2023-02-22 12:01:10 +0530

    ci

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -25,10 +25,10 @@ def lintCheck() {
 def call() {
 pipeline {
     agent any 
-     environment {
-        SONAR      = credentials('SONAR')
-       NEXUS      = credentials('NEXUS')
-    }
+     //environment {
+       // SONAR      = credentials('SONAR')
+       //NEXUS      = credentials('NEXUS')
+    //}
       stages{
         stage ('Downloading the Dependencies') {
               steps{
